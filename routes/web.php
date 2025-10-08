32f581522a93ac6164cd7aaaeeaf80d97579ec7f
bella <143981370+bellaacp@users.noreply.github.com>
2025-10-08 07:44:32 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use app\Http\Controllers\ProfileController;
 use App\Http\Controllers\NinjaController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,3 +12,15 @@ Route::get('/ninjas/create', [NinjaController::class, 'create'])->name('ninjas.c
 Route::get('/ninjas/{ninja}', [NinjaController::class, 'show'])->name('ninjas.show');
 Route::post('/ninjas', [NinjaController::class, 'store'])->name('ninjas.store');
 Route::delete('/ninjas/{ninja}', [NinjaController::class, 'destroy'])->name('ninjas.destroy');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
